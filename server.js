44a53e3e30d5903862ac21e5bb5edcf6d3123ccd
gazi-aycil <gaziaycil27@gmail.com>
2025-06-05 17:09:46 +0300

    fix for add image for subcategory

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const imagekit = new ImageKit({
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Configure multer for memory storage
 const storage = multer.memoryStorage();
